4f26faab9396aadee96471b97c853734cd06e55b
Adedipupo <bayonleolaluwa@gmail.com>
2020-10-23 11:32:47 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import './App.css';
-import Navbar from './components/layouts/Navbar';
-import Index from './components/layouts/Index';
-import { Router, Route, Switch  } from 'react-router';
+ import Navbar from './components/layouts/Navbar';
+// import { Router, Route, Switch  } from 'react-router-dom';
+// import Index from './components/layouts/Index';
 
 
 
-class App extends React.Component{
+class App extends Component{
   render(){
     return(
-      <Router>
-        <React.Fragment>
-          <Navbar />
-
-          <div className="container">
-             <Switch>
-               <Route exact path="/" component={Index} />
-             </Switch>
-          </div>
-        </React.Fragment>
-      </Router>
+      <div className="App">
+        <Navbar />
+      </div>
     );
   }
 }
