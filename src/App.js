99ac6d95234587c641fead421436583fec122e3c
Adedipupo <bayonleolaluwa@gmail.com>
2020-10-23 12:25:23 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React,{ Component } from 'react';
 import './App.css';
- import Navbar from './components/layouts/Navbar';
-// import { Router, Route, Switch  } from 'react-router-dom';
-// import Index from './components/layouts/Index';
+import {
+  Switch,
+  Route, BrowserRouter
+} from "react-router-dom";
+ import Index from './components/layouts/Index';
+import Navbar from './components/layouts/Navbar';
 
 
 
 class App extends Component{
   render(){
     return(
-      <div className="App">
+      <>
+        <BrowserRouter>
         <Navbar />
-      </div>
+          <div className="container">
+            <Switch>
+              <Route exact path="/index">
+                <Index />
+              </Route>
+            </Switch>
+          </div>
+
+        </BrowserRouter>
+      </>
     );
   }
 }
--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Navbar = () => {
 return(
-  <nav className="nabvar navbar-dark bg-dark mb-5">
-     <span className="navbar-brand mb-0 h1 mx-auto">LyricsFinder</span>
-  </nav>
+  <div className="App">
+    <nav className="nabvar navbar-dark bg-dark mb-5">
+      <span className="navbar-brand mb-0 h1 mx-auto">LyricsFinder</span>
+    </nav>
+  </div>
 )
 }
 
