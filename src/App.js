fd22bcb103a5eeaa9cbaf5e2c5c067dd8d7b7d02
Adedipupo <bayonleolaluwa@gmail.com>
2020-10-23 01:04:26 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/layouts/Navbar';
+import { Router, Route, Switch  } from 'react-router';
 
 
 
 class App extends React.Component{
   render(){
     return(
-      <div className="App">
-       <Navbar />
-      </div>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+
+          <div className="container">
+             <Switch>
+               <Route exact path="/" component={Index} />
+             </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
     );
   }
 }
