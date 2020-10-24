d7ff4033e234adbe1b372ffdde7bca49e8ea07e3
Adedipupo <bayonleolaluwa@gmail.com>
2020-10-24 02:09:21 +0100

    wip

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { Consumer } from '../../context';
+import Spinner from '../layouts/Spinner';
 
 class Tracks extends Component{
 
@@ -7,8 +8,22 @@ class Tracks extends Component{
     return(
       <Consumer>
         {value => {
-          console.log(value);
-          return <h1>Tracks</h1>
+          const { track_list , heading} = value;
+
+          if (track_list === undefined || track_list.length === 0) {
+            return <Spinner />
+          } else {
+            return(
+              <React.Fragment>
+                <h3 className="text-center mb-4">{ heading }</h3>
+                <div className="row">
+                  { track_list.map(item => (
+                    <Track />
+                  ))}
+                </div>
+              </React.Fragment>
+            )
+          }
         }}
       </Consumer>
     )
