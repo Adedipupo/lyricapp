84663fc87be877fca30a7326f88a88120f857587
Adedipupo <bayonleolaluwa@gmail.com>
2020-10-26 10:54:51 +0100

    wip

---
+++ src/components/tracks/Lyrics.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Lyrics extends Component{
+  state = {
+
+  }
+  
+  render(){
+    return(
+      <div>
+         
+      </div>
+    )
+  }
+}
+
+export default Lyrics
