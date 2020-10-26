da335fafeef20e1d76e54a8673fa66155a281042
Adedipupo <bayonleolaluwa@gmail.com>
2020-10-26 14:37:33 +0100

    wip

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -14,31 +14,53 @@ class Lyrics extends Component {
     )
       .then((res) => {
         //     console.log(res.data)
-        this.setState({lyrics: res.data.message.body.lyrics });
-          return Axios.get(
-              `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
-          )
+        this.setState({ lyrics: res.data.message.body.lyrics });
+        return Axios.get(
+          `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
+        );
       })
-      .then(res => {
-          this.setState({ track: res.data.message.body.track})
+      .then((res) => {
+        this.setState({ track: res.data.message.body.track });
       })
       .catch((err) => console.log(err));
   }
 
   render() {
-   const { track, lyrics } = this.state;
-      if (track === undefined || lyrics === undefined || Object.keys(track).length === 0 || Object.keys(lyrics).length === 0) {
-       return <Spinner />
-   } else {
-       <React.Fragment>
-           <Link to="/" className="btn btn-dark btn-sm mb-4">Go Back</Link>
-           <div className="card">
-            <div className="card-header">
-   {track.track_name} by <span className="text-secondary">{track.artist_name}</span>
+    const { track, lyrics } = this.state;
+    if (
+      track === undefined ||
+      lyrics === undefined ||
+      Object.keys(track).length === 0 ||
+      Object.keys(lyrics).length === 0
+    ) {
+      return <Spinner />;
+    } else {
+      return (
+        <React.Fragment>
+          <Link to="/" className="btn btn-dark btn-sm mb-4">
+            Go Back
+          </Link>
+          <div className="card">
+            <h5 className="card-header">
+              {track.track_name} by{""}
+              <span className="text-secondary">{track.artist_name}</span>
+            </h5>
+            <div className="card-body">
+              <p className="card-text">{lyrics.lyrics_body}</p>
             </div>
-           </div>
-       </React.Fragment>
-   }
+          </div>
+
+          <ul className="list-group mt-3">
+            <li className="list-group-item">
+              <strong>Album ID</strong> : {track.album_id}
+            </li>
+            <li className="list-group-item">
+              <strong>Song Genre</strong> : {track.primary_genres.music_genre_list[0].music_genre.music_genre_name}
+            </li>
+          </ul>
+        </React.Fragment>
+      );
+    }
   }
 }
 
