459e57647889e8be92d9a579aed0fb22f7560993
Adedipupo <bayonleolaluwa@gmail.com>
2020-10-26 13:10:49 +0100

    wip

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -14,18 +14,22 @@ class Lyrics extends Component {
         //     console.log(res.data)
         this.setState({lyrics: res.data.message.body.lyrics });
           return Axios.get(
-              `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
+              `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
           )
+      })
+      .then(res => {
+          this.setState({ track: res.data.message.body.track})
       })
       .catch((err) => console.log(err));
   }
 
   render() {
-    return (
-      <div>
-        <h1>Lyricsss</h1>
-      </div>
-    );
+   const { track, lyrics } = this.state;
+   if (track === underfined) {
+       
+   } else {
+       
+   }
   }
 }
 
